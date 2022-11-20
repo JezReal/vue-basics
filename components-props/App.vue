910c2ef600ef203f3df7dd02afa6bf56aea49a97
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 15:16:53 +0800

    add components

---
+++ components-props/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MyComponent from "./MyComponent.vue";
+
+const dynamicTitle = "dynamic title";
+</script>
+
+<template>
+  <div>
+    <h1>Components</h1>
+    <my-component title="title one" description="description one" />
+    <my-component title="title two" description="description two" />
+    <my-component :title="dynamicTitle" description="description three" />
+    <my-component :title="dynamicTitle" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components-props/MyComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string;
+  description?: string;
+}>();
+</script>
+
+<template>
+  <div>
+    <h3>{{ props.title }}</h3>
+    <h5>{{ props.description ?? "Default description" }}</h5>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: 1px solid gray;
+  margin: 5px;
+}
+</style>
