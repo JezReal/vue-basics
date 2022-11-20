43dcbb31a06214e6b18e853c24afbf6c13589924
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 14:06:57 +0800

    add template syntax

---
+++ template-syntax/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = "Goodbye owrld";
+const name = "Jezreel";
+
+function hasReturnValue() {
+  return "I have a value";
+}
+
+function noReturnValue() {
+  console.log("I have no value");
+}
+
+const dynamicId = "2";
+const dynamicClass = ref("default");
+
+function removeClass() {
+  dynamicClass.value = null;
+}
+
+function addClass() {
+  dynamicClass.value = "default";
+}
+</script>
+
+<template>
+  <div>
+    <h3>
+      {{ message }}
+    </h3>
+
+    <p>{{ name }}</p>
+    <p>{{ 1 + 12 }}</p>
+    <p>{{ 1 - 12 }}</p>
+
+    <p>{{ hasReturnValue() }}</p>
+    <p>{{ noReturnValue() }}</p>
+
+    <h3 v-bind:id="dynamicId">I have a dynamic id</h3>
+    <h3 v-bind:class="dynamicClass" class="static-class">
+      I have a dynamic id
+    </h3>
+
+    <button @click="removeClass">Remove class</button>
+    <button @click="addClass">add class</button>
+  </div>
+</template>
+
+<style scoped></style>
