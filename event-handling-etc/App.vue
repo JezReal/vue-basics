e1ceac8208c4f2c4e2c1f3dde1f844098ed4d788
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 14:42:51 +0800

    add event handling

---
+++ event-handling-etc/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(1);
+const inputValue = ref("Initial value");
+</script>
+
+<template>
+  <div>
+    <h1>Count: {{ count }}</h1>
+    <button @click="count++">Increment</button>
+
+    <br />
+    <br />
+
+    <p>{{ inputValue }}</p>
+    <input type="text" placeholder="Enter text" v-model="inputValue" />
+  </div>
+</template>
+
+<style scoped></style>
