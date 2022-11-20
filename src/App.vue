77ab2f9054f43c968e1a8562337597b22cf50476
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 15:45:32 +0800

    add sample app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,66 @@
 <script setup lang="ts">
-let message = "Hello world";
+import { ref } from "vue";
+import type { ArtistWithIdDto } from "./ArtistWIthIdDto";
+import CustomCard from "./CustomCard.vue";
+
+const artists = ref<ArtistWithIdDto[]>();
+let cachedArtists: ArtistWithIdDto[];
+
+const searchTerm = ref("");
+
+function searchArtists() {
+  if (searchTerm.value.length > 0) {
+    artists.value = cachedArtists.filter((artist) => {
+      return artist.artist_name.toLowerCase().includes(searchTerm.value);
+    });
+  } else {
+    artists.value = cachedArtists;
+  }
+}
+
+fetch("https://localhost:7068/api/artists")
+  .then((response) => response.json())
+  .then((data) => {
+    artists.value = data;
+    cachedArtists = data;
+  });
 </script>
 
 <template>
-  <div><h1>Vue js basics</h1></div>
+  <div class="parent">
+    <h2>Cool Artists</h2>
+    <input
+      type="text"
+      placeholder="Search artist"
+      v-model="searchTerm"
+      @input="searchArtists"
+    />
+
+    <div v-for="artist in artists" :key="artist.id" class="cards">
+      <custom-card :artist="artist" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h2 {
+  font-size: 40px;
+  text-align: center;
+}
+.cards {
+  width: 75%;
+}
+
+.parent {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  height: 30px;
+  width: 500px;
+  border: 2px solid black;
+  border-radius: 5px;
+}
+</style>
