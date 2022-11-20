7da00f9c350609299e0257fd0442606f5e5b9715
JezReal <jezreeljoshuam@gmail.com>
2022-11-20 15:08:18 +0800

    add list rendering

---
+++ list-rendering/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const names = ref([
+  {
+    id: 1,
+    name: "Jezreel",
+  },
+  {
+    id: 2,
+    name: "Joshua",
+  },
+  {
+    id: 3,
+    name: "Martin",
+  },
+]);
+</script>
+
+<template>
+  <div v-for="name in names" :key="name.id">
+    <ul>
+      <li>{{ name.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
